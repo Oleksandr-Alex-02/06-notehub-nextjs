819162929690cc5eaaf6cca3ae21b0f60a77a328
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-08-24 12:41:53 +0300

    Delete

--- app/Notes/[NotesId]/page.tsx
+++ app/Notes/[NotesId]/page.tsx
@@ -17,28 +17,3 @@ export default async function TaskDetailsPage({ params }: Props) {
         </div>
     );
 }
-
-
-// type Props = {
-//     params: Promise<{ id: string }>;
-// };
-
-// const NoteDetails = async ({ params }: Props) => {
-//     const { id } = await params;
-//     const note = await getSingleNote(id);
-
-//     const formattedDate = note.updatedAt
-//         ? `Updated at: ${note.updatedAt}`
-//         : `Created at: ${note.createdAt}`;
-
-//     return (
-//         <div>
-//             <h2>{note.title}</h2>
-//             <p>{note.content}</p>
-//             <button>Edit</button>
-//             <p>{formattedDate}</p>
-//         </div>
-//     );
-// };
-
-// export default NoteDetails;
--- components/NoteItem/NoteItem.tsx
+++ components/NoteItem/NoteItem.tsx
@@ -2,19 +2,36 @@
 import Link from "next/link";
 import { Note } from "@/types/note";
 import css from "./NoteItem.module.css"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "@/lib/api";
 
 type Props = {
     item: Note;
 };
 
 export default function NoteItem({ item }: Props) {
+    const queryClient = useQueryClient();
+    const mutation = useMutation({
+        mutationFn: () => deleteNote(item.id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["notes"] });
+        },
+    });
+
     return (
         <li className={css.listItem} key={item.id}>
             <h2 className={css.title}>{item.title}</h2>
             <p className={css.content}>{item.content}</p>
             <div className={css.footer}>
                 <span className={css.tag}>{item.tag}</span>
-                <button className={css.button}>Delete</button>
+                {/* <button className={css.button}>Delete</button> */}
+                <button
+                    className={css.button}
+                    onClick={() => mutation.mutate()}
+                    disabled={mutation.isLoading}
+                >
+                    {mutation.isLoading ? "Deleting..." : "Delete"}
+                </button>
             </div>
             <Link href={`/notes/${item.id}`}>{item.title}</Link>
         </li>
